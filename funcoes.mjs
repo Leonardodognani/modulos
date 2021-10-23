0c80c676d80b0fcf6bfc70c957b8f973e34b3ac3
Leonardodognani <82122343+Leonardodognani@users.noreply.github.com>
2021-10-23 14:55:31 -0300

    add first one

---
+++ funcoes.mjs
@@ -0,0 +1,13 @@
+function mostraIdade(nome, idade) {
+    console.log(`A idade de ${nome} é ${idade}.`);
+}
+
+function mostraCidade(nome, cidade) {
+    console.log(`A Cidade de ${nome} é ${cidade}.`);
+}
+
+function mostraHobby(nome, hobby) {
+    console.log(`O Hobby de ${nome} é ${hobby}.`);
+}
+
+export { mostraIdade, mostraCidade, mostraHobby };
---
+++ main.mjs
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+
+console.log(mostraIdade("Leonardo", "36"));
+console.log(mostraCidade("Leonardo", "Piracicaba"));
+console.log(mostraHobby("Leonardo", "Ler Livros"));
